52fd8813d20c031a27be2109c9101ed6014ae7c1
AlexandraViorel <alexsig22@yahoo.com>
2023-05-19 15:27:40 +0300

    configs that work now

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,26 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { AppMenu } from './components/AppMenu';
-import { AppHome } from './components/AppHome';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RegistrationForm } from './components/auth/Register';
 import { ActivateAccount } from './components/auth/Activate';
 import { LoginForm } from './components/auth/Login';
 import { LogoutForm } from './components/auth/Logout';
+import { AppMenu } from './components/AppMenu';
+import { AppHome } from './components/AppHome';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
     <React.Fragment>
       <Router>
         <AppMenu/>
         <Routes>
           <Route path="/" element={<AppHome />} />
-
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/activate/:activationCode" element={<ActivateAccount />} />
           <Route path="/login" element={<LoginForm />} />
@@ -29,8 +48,7 @@ function App() {
         </Routes>
       </Router>
     </React.Fragment>
-
   )
 }
 
-export default App;
+export default App
